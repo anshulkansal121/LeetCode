1a26836289801dd1196f1de4b7db4116636523d4
Anshul Kansal <61547386+anshulkansal121@users.noreply.github.com>
2024-08-08 10:59:24 +0530

    Time: 98 ms (39.94%), Space: 54 MB (7.06%) - LeetHub

--- 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.cpp
+++ 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.cpp
@@ -1,15 +1,28 @@
 class Solution {
 public:
     int subarraySum(vector<int>& nums, int k) {
-        // BRUTE FORCE
-        int cnt = 0;
-        for (int i = 0; i < nums.size(); i++) {
-            int sum = 0;
-            for (int j = i; j < nums.size(); j++) {
-                sum += nums[j];
-                if (sum == k)
-                    cnt++;
-            }
+        // BETTER Approach
+        // int cnt = 0;
+        // for (int i = 0; i < nums.size(); i++) {
+        //     int sum = 0;
+        //     for (int j = i; j < nums.size(); j++) {
+        //         sum += nums[j];
+        //         if (sum == k)
+        //             cnt++;
+        //     }
+        // }
+        // return cnt;
+
+        //OPTIMAL Approach
+        map<int,int>mp;
+        mp[0] = 1;
+        int prefixSum = 0, cnt= 0;
+        for(int i = 0;i<nums.size();i++)
+        {
+            prefixSum+=nums[i];
+            int remove = prefixSum - k;
+            cnt+=mp[remove];
+            mp[prefixSum]++;
         }
         return cnt;
     }
