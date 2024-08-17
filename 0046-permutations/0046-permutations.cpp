62a021d5cee0b9cfd42434c8c58e2c2f015d3a04
Anshul Kansal <61547386+anshulkansal121@users.noreply.github.com>
2024-08-17 17:58:02 +0530

    Time: 0 ms (100%), Space: 9.1 MB (58.09%) - LeetHub

--- 0046-permutations/0046-permutations.cpp
+++ 0046-permutations/0046-permutations.cpp
@@ -2,32 +2,54 @@ class Solution {
 public:
     //APPROACH 1
     //Using MAP DS to store the index which have been traversed / Added to the permutation
-    void permutation(unordered_map<int,int>& mp, vector<int>& nums, vector<vector<int>>& ds, vector<int>& temp)
+    // void permutation(unordered_map<int,int>& mp, vector<int>& nums, vector<vector<int>>& ds, vector<int>& temp)
+    // {
+    //     if(temp.size()==nums.size())
+    //     {
+    //         ds.push_back(temp);
+    //         return;
+    //     }
+    //     for(int i = 0;i<nums.size();i++)
+    //     {
+    //         if(!mp[i])
+    //         {
+    //             mp[i] = 1;
+    //             temp.push_back(nums[i]);
+    //             permutation(mp,nums,ds,temp);
+    //             temp.pop_back();
+    //             mp[i] = 0;
+    //         }
+    //     }
+        
+    // }
+    void permutation(int ind, vector<int>& nums, vector<vector<int>>& ans)
     {
-        if(temp.size()==nums.size())
+        //Base Condition:
+        if(ind == nums.size())
         {
-            ds.push_back(temp);
+            ans.push_back(nums);
             return;
         }
-        for(int i = 0;i<nums.size();i++)
+        for(int i = ind;i<nums.size();i++)
         {
-            if(!mp[i])
-            {
-                mp[i] = 1;
-                temp.push_back(nums[i]);
-                permutation(mp,nums,ds,temp);
-                temp.pop_back();
-                mp[i] = 0;
-            }
+            swap(nums[i],nums[ind]);
+            permutation(ind+1,nums,ans);
+            swap(nums[i],nums[ind]);
         }
-        
     }
     vector<vector<int>> permute(vector<int>& nums) {
-        vector<vector<int>> ds;
-        vector<int> temp;
-        unordered_map<int,int> mp;
-        permutation(mp,nums,ds,temp);
-        return ds;
+
+        // Approach 1:
+        // vector<vector<int>> ds;
+        // vector<int> temp;
+        // unordered_map<int,int> mp;
+        // permutation(mp,nums,ds,temp);
+        // return ds;
+
+        //Approach 2:
+        vector<vector<int>> ans;
+        permutation(0,nums,ans);
+        return ans;
     }
 };
 
