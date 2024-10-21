9a98dbe177ee13d9aeb29c918b0ab538dcd0497a
Anshul Kansal <61547386+anshulkansal121@users.noreply.github.com>
2024-10-21 10:40:24 +0530

    Time: 231 ms (58.19%), Space: 47.8 MB (81.58%) - LeetHub

---
+++ 1715-split-a-string-into-the-max-number-of-unique-substrings/1715-split-a-string-into-the-max-number-of-unique-substrings.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    int maxUniqueSplit(string s) {
+        unordered_set<string> st;
+        return backtrack(s, 0, st);
+    }
+
+private:
+    int backtrack(const string& s, int start, unordered_set<string>& st) {
+
+        if (start == s.size())
+            return 0;
+
+        int maxCount = 0;
+
+        for (int end = start + 1; end <= s.size(); ++end) {
+            string substring = s.substr(start, end - start);
+            if (st.find(substring) == st.end()) {
+                st.insert(substring);
+                maxCount = max(maxCount, 1 + backtrack(s, end, st));
+                st.erase(substring);
+            }
+        }
+        return maxCount;
+    }
+};
