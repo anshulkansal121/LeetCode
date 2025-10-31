35b5a2685e59466630ba049ba6c632b18f6c0069
Anshul Kansal <61547386+anshulkansal121@users.noreply.github.com>
2025-10-31 09:39:31 +0530

    Time: 301 ms (79.78%), Space: 67.6 MB (78.25%) - LeetHub

---
+++ 2024-calculate-special-bonus/2024-calculate-special-bonus.py
@@ -0,0 +1,9 @@
+import pandas as pd
+
+def calculate_special_bonus(employees: pd.DataFrame) -> pd.DataFrame:
+    employees['bonus'] = 0
+    employees.loc[(employees['employee_id'] % 2 != 0) & (~employees['name'].str.startswith('M')),'bonus'] = employees['salary']
+    return employees.sort_values('employee_id')[['employee_id','bonus']]
+    
+
+    
