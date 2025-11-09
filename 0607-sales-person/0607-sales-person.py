7d63a368ff38ae1955daf8db6eee991cd0cd2aba
Anshul Kansal <61547386+anshulkansal121@users.noreply.github.com>
2025-11-09 09:59:26 +0530

    Time: 349 ms (84.18%), Space: 67.5 MB (96.86%) - LeetHub

---
+++ 0607-sales-person/0607-sales-person.py
@@ -0,0 +1,7 @@
+import pandas as pd
+
+def sales_person(sales_person: pd.DataFrame, company: pd.DataFrame, orders: pd.DataFrame) -> pd.DataFrame:
+    com_red = company[(company['name']=='RED')]['com_id']
+    order_red = orders[orders['com_id'].isin(com_red)]['sales_id']
+    return sales_person[~(sales_person['sales_id'].isin(order_red))][['name']]
+    
